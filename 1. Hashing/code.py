169a54cd5b55ff705af23f4f53b91e4930c852f5
Nycto-c05 <96388616+Nycto-c05@users.noreply.github.com>
2023-05-20 19:23:13 +0530

    Update code.py

--- 1. Hashing/code.py
+++ 1. Hashing/code.py
@@ -1,46 +1,102 @@
-hashTable = [-1 for i in range(10)]
-count = 0
+HashTable = [-1 for i in range(10)]
 size = 10
+count = 0
+comparison = 0
+compareForquad = 0
 
-def hasF(data):
-    key = data%size
-    return key;
+def hashFunc(val):
+    key = val % size
+    return key
 
-def insert():
+def insert(data):
+    key = hashFunc(data)
+    global count
+    if (count == size):
+        print("Hash Table is full!! ")
+        return
 
-    global count, size;
-
-    if(count == size):
-        print("Hash table is full");
+    if (HashTable[key] == -1):
+        HashTable[key] = data
+        count += 1
     else:
-        data = int(input("Enter the data: "))
-        key = hasF(data)
+        key = quadraticProb(key)
+        HashTable[key] = data
+        count += 1
 
-        if(hashTable[key] == -1):
-            hashTable[key] = data
-            count+=1
-        else:
-            QuadProb(data, key)
-            count+=1
+def linearProbing(key):
+    global comparison
+    while (HashTable[key] != -1):
+        key  = (key + 1) % size
+    return key
 
-def linearProbing(data, key):
-    global size, count
-    while(hashTable[key]!= -1):
-        key = (key + 1)%size
-    hashTable[key] = data
-
-def QuadProb(data, key):
-    global size, count
-    i = 1
+def quadraticProb(key):
+    global HashTable
     curr = key
-    while(hashTable[curr]!= -1):
-        newkey = (key + (i**2))%size
-        curr = newkey
-        i+=1
+    i = 1
+    while (HashTable[curr] != -1):
+        curr = (key + i*i) % size
+        i += 1
+    return curr
 
-    hashTable[newkey] = data
+     
+def linearProbingForSearch(key, data):
+    global comparison
+    while (HashTable[key] != data):
+        key  = (key + 1) % size
+        comparison += 1
+        if comparison > size :
+            print (f"{data} not found! ")
+            return comparison
+        
+    print(f"{data} is present in the hashtable at index {key}")
+    return comparison
 
-for i in range(5):
-    insert()    
+def quadraticProbingForSearch(key, data):
+    global HashTable, compareForquad
+    curr = key
+    i = 1
+    while (HashTable[curr] != data):
+        curr = (key + i*i) % size
+        i += 1
+        compareForquad += 1
+        if compareForquad > size:
+            print (f"{data} not found! ")
+            return compareForquad
+        
+    print(f"{data} is present in the hashtable at index {curr}")
+    return compareForquad
 
-print(hashTable)
+
+def search(data):
+    key = hashFunc(data)    
+    global comparison, compareForquad
+    comparison += 1
+    compareForquad += 1
+
+    if HashTable[key] == data :
+        print(f"{data} is present in the hashtable at index {key}")   
+        print("No. of comparisons made is 1")     
+    else:
+        print("Searching using linear probing.........")
+        linearProbingForSearch(key, data)
+        print(f"No. of comparisons made in linear probing are {comparison}")
+
+        print("\nSearching using quadratic probing.........")
+        quadraticProbingForSearch(key, data)
+        print(f"No. of comparisons made in quadratic probing are {compareForquad}")
+
+
+insert(11)
+insert(33)
+insert(43)
+insert(81)
+insert(56)
+insert(79)
+insert(10)
+insert(99)
+insert(45)
+insert(53)
+print(HashTable)
+
+s = int(input("Enter the data to be searched: "))
+search(s)
